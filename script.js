834a75fe1f0d3954e1ba9c5d7e13170426b7fd23
juangagna <jgagna@itba.edu.ar>
2022-10-25 20:17:54 -0300

    b

---
+++ script.js
@@ -0,0 +1,12 @@
+let Total = 0;
+console.log("Hola");
+function sumarprecio(valorproducto) {
+  Total = Total + valorproducto;
+  document.getElementById("Total").innerHTML =
+    "TOTAL: " + "$" + Total + " Pesos";
+}
+function Borrar() {
+  Total = 0;
+  document.getElementById("Total").innerHTML = "TOTAL:" + " $" + 0 + " Pesos";
+}
+
